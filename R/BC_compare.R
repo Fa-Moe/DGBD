d56c46f8f1c237a8afece3c890de413190e2d069
Fa-Moe <125106860+Fa-Moe@users.noreply.github.com>
2024-03-06 20:53:17 -0600

    Added BC_compare function that allows error visualization.

---
+++ R/BC_compare.R
@@ -0,0 +1,63 @@
+#' Compare the linear and the nls methods parameters
+#'
+#' Creates a graph that illustrates error in the predictions of the linear and nls models for quick comparison. The y axis is the difference between the predicted value and the original abundance value. This value is then made absolute and 1 is added before taking its logarithm. The addition of 1 keeps a difference of 0 as 0 in the plot and prevents a small decimal linear difference from becoming a large difference in the plot. The x axis is the rank for that given error. This allows us to see where in the distribution each model performs better or worse.
+#'
+#'
+#' @param df_abundance A data frame that contains abundance data.
+#' @param column_compare Either a string with the name of the column or the number of the column that stores the abundances in the data frame.
+#' @param BC_plot_list A list that contains 2 objects previously generated with \code{BC_plot}. The first one must use the linear paramenters and the second one parameters estimated by the nls method.
+#' @param c_gfx_title String. Changes the title of the graph.
+#' @param c_gfx_label Logical. Adds a label that adds the R2 of both models. Defaults to true.
+#' @param ... passes arguments to \code{BC_plot}.
+#'
+#' @return A list with that includes a data frame with difference data between predicted and real values and
+#' @export
+#'
+#' @examples
+#' BC_compare(hmp_wgs,2)
+#'
+
+BC_compare <- function(df_abundance=NULL,column_compare=NULL,BC_plot_list=NULL,c_gfx_title="Linear vs nls model error",c_gfx_label=TRUE,...){
+  if(!is.null(BC_plot_list)){
+    naive <- BC_plot_list[[1]]
+    nls_obj <- BC_plot_list[[2]]
+    }
+  else if (!is.null(df_abundance)){
+    naive <- BC_plot(df_abundance,column_plot=column_compare,...)
+    nls_obj <- BC_plot(df_abundance,column_plot=column_compare,nls=T,...)
+    }
+  else {stop("Neither an abundance data frame or a BC_plot_list were provided")}
+  ys_data <-  data.frame(
+    y_real = naive[[1]][,"abundance"],
+    y_linear = naive[[1]][,"predicted_values"],
+    y_nls =  nls_obj[[1]][,"predicted_values"],
+    x_real = naive[[1]][,"BC_rank"]
+  )
+
+  ys_diffs <- c(t(log10(abs(ys_data[,"y_linear"]-ys_data[,"y_real"])+1)),t(log10(abs(ys_data[,"y_nls"]-ys_data[,"y_real"])+1)))
+  ys_diff_type <- c(rep("linear",length(ys_data[,"y_linear"])), rep("nls",length(ys_data[,"y_nls"])))
+  diff_data <-  as.data.frame(cbind(c(naive[[1]][,"BC_rank"],naive[[1]][,"BC_rank"]),ys_diffs,ys_diff_type))
+  colnames(diff_data) <- c("Rank","Difference","Type")
+  linear_r2 <- stats::cor(ys_data[,"y_real"],ys_data[,"y_linear"])^2
+  nls_r2 <- stats::cor(ys_data[,"y_real"],ys_data[,"y_nls"])^2
+
+  grafx_compare <- ggplot2::ggplot(data=diff_data,ggplot2::aes(x=as.numeric(diff_data[,"Rank"]),y=as.numeric(diff_data[,"Difference"]),group=diff_data[,"Type"],col=diff_data[,"Type"]))+
+    ggplot2::geom_line()+
+    ggplot2::scale_color_manual(values=c("#61C906","#B606C9"))+
+    ggplot2::labs(title=c_gfx_title,
+                  x="Rank",
+                  y="log10(Error+1)")+
+    ggplot2::annotate("label",
+                      x = max(as.numeric(diff_data[,"Rank"]))*4/5,
+                      y = max(as.numeric(diff_data[,"Difference"]))*4/5,
+                      label =
+                        if (c_gfx_label){paste("R2 value of the different models",
+                                             "\n","linear =",format(round(linear_r2, 4), nsmall = 4),
+                                             "\n","nls =",format(round(nls_r2, 4), nsmall = 4)
+                                             )}
+                      else {NULL},
+                      size=3)
+
+  return(list(grafx_compare,diff_data))
+  }
+
